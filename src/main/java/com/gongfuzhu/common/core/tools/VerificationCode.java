5921647dcfd001c426926300a33fad421651ede7
gongfuzhu <814610491>
2022-02-04 23:03:24 +0800

    。

--- src/main/java/com/gongfuzhu/common/core/tools/VerificationCode.java
+++ src/main/java/com/gongfuzhu/common/core/tools/VerificationCode.java
@@ -1,7 +1,8 @@
 package com.gongfuzhu.common.core.tools;
 
-import org.springframework.boot.configurationprocessor.json.JSONException;
-import org.springframework.boot.configurationprocessor.json.JSONObject;
+import com.jayway.jsonpath.JsonPath;
+import lombok.extern.log4j.Log4j2;
+import org.springframework.stereotype.Component;
 import org.springframework.util.Assert;
 
 import java.util.Map;
@@ -11,7 +12,8 @@ import java.util.regex.Pattern;
 /**
  * https://www.showdoc.com.cn/1723788762180300/8055380618566522
  */
-
+@Component
+@Log4j2
 public class VerificationCode {
 
     /**
@@ -19,17 +21,12 @@ public class VerificationCode {
      *
      * @return
      */
-    public  String login() {
+    public static String login() {
 
         String requesGet = HttpClientTool.requesGet("http://api.weilai.best/login?username=gongfuzhu&password=admin123456");
         String token = null;
-        try {
-            JSONObject jsonObject = new JSONObject(requesGet);
-            Assert.isTrue(jsonObject.get("code").equals("ok"), "业务异常");
-            token = (String) jsonObject.get("token");
-        } catch (JSONException e) {
-            e.printStackTrace();
-        }
+        token = JsonPath.read(requesGet, "$.token");
+        Assert.isTrue(JsonPath.read(requesGet, "$.code").equals("ok"), "业务异常");
 
         return token;
     }
@@ -40,78 +37,79 @@ public class VerificationCode {
      * @param token
      * @return
      */
-    public  String getmoney(String token) {
+    public static String getmoney(String token) {
 
         String url = String.format("http://api.weilai.best/getmoney?token=%s", token);
         String requesGet = HttpClientTool.requesGet(url);
         String account = null;
-        try {
-            JSONObject jsonObject = new JSONObject(requesGet);
-            Assert.isTrue(jsonObject.get("code").equals("ok"), "业务异常");
-            account = (String) jsonObject.get("money");
-        } catch (JSONException e) {
-            e.printStackTrace();
-        }
+        Assert.isTrue(JsonPath.read(requesGet, "$.code").equals("ok"), "业务异常");
+        account = JsonPath.read(requesGet, "$.money");
         return account;
 
     }
 
     /**
      * {"code":"ok","requestid":"236573","phone":"15607045713","residual":"149"}
+     *
      * @param token
      * @return
      */
-    public  String getPhone(String token,String productId) throws JSONException {
+    public static String getPhone(String token, String productId){
 
 
-        String url = "http://app.weilai.best/getphone?token=" + token + "&id="+productId+"&operator=1&province=%E4%B8%8D%E9%99%90&phone=&autoblack=true";
+        String url = "http://app.weilai.best/getphone?token=" + token + "&id=" + productId + "&operator=1&province=%E4%B8%8D%E9%99%90&phone=&autoblack=true";
 
         String requesGet = HttpClientTool.requesGet(url);
-            JSONObject jsonObject = new JSONObject(requesGet);
-            String residual = (String) jsonObject.get("residual");
-            Assert.isTrue(!residual.equals("0"), "residual 值为0 停止使用");
+        Assert.isTrue(!JsonPath.read(requesGet,"$.residual").equals("0"), "residual 值为0 停止使用");
 
         return requesGet;
 
     }
 
-    public  String getmessage(String token, String requestid) throws JSONException {
+    public static String getmessage(String token, String requestid) {
 
         String url = String.format("http://api.weilai.best/getmessage?token=%s&requestid=%s", token, requestid);
-        for (int i = 0; i < 10; i++) {
+        for (int i = 0; i < 20; i++) {
             String requesGet = HttpClientTool.requesGet(url);
-            JSONObject jsonObject = new JSONObject(requesGet);
-            if (jsonObject.get("code").equals("ok") && !String.format((String) jsonObject.get("sms")).isEmpty()) {
-                return String.format((String) jsonObject.get("sms"));
+            if (JsonPath.read(requesGet,"$.code").equals("ok") && !JsonPath.read(requesGet,"$.sms").equals("")) {
+                return JsonPath.read(requesGet,"$.sms");
             }
             try {
-                Thread.sleep(1000);
+                Thread.sleep(2000);
             } catch (InterruptedException e) {
                 e.printStackTrace();
             }
 
         }
+        freePhone(token, requestid);
         return null;
     }
 
-    public  Map<String, String> aiwenzhe() throws JSONException {
-        String token = login();
-        String phone = getPhone(token,"22819");
-        JSONObject jsonObject = new JSONObject(phone);
-        Object requestid = jsonObject.get("requestid");
-        Object no = jsonObject.get("phone");
-        System.out.println("电话："+no);
-        String getmessage = getmessage(token, String.valueOf(requestid));
-        String code = code(getmessage, 6);
-       return Map.of("phone",String.valueOf(no),"code",code);
+    public static void freePhone(String token, String requestid) {
+        log.info("释放手机号");
+        String url = String.format("http://api.weilai.best/freephone?token=%s&requestid=%s", token, requestid);
+        HttpClientTool.requesGet(url);
+
     }
 
-//    public static void main(String[] args) throws JSONException {
-//        Map<String, String> aiwenzhe = aiwenzhe();
-//        System.out.println(aiwenzhe.get("phone"));
-//        System.out.println(aiwenzhe.get("code"));
-//    }
-    private  String code(String body, int lenth) {
+    public static Map<String, String> aiwenzhe() {
+        String token = login();
+        String phone = getPhone(token, "22819");
+
+
+        String requestid = JsonPath.read(phone,"$.requestid");
+        String no = JsonPath.read(phone,"$.phone");
+
+        return Map.of("phone", String.valueOf(no), "requestid", String.valueOf(requestid));
+    }
+
+    public static String aiwenzheCode(String token, String requestid){
+        String getmessage = getmessage(token, String.valueOf(requestid));
+        return code(getmessage, 6);
+
+    }
+
+    private static String code(String body, int lenth) {
         Pattern p = Pattern.compile("(?<![0-9])([0-9]{" + lenth + "})(?![0-9])");
         Matcher m = p.matcher(body);
         if (m.find()) {
